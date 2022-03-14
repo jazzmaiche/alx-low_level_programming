25d6387abf2344971257f7e31177a3e0fdc3f525
jazzmaiche <jazzmaniche@gmail.com>
2022-03-14 15:41:17 -0700

    My 03 variable

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return
+ */
+int main(void)
+{
+char lower_case;
+char upper_case;
+
+for (lower_case = 'a'; lower-lower <= 'z'; lower_case++)
+{
+putchar(lower_case);
+}
+
+for (upper_case = 'A'; upper _case <= 'z' ; upper_case++)
+{
+putchar(upper_case++);
+}
+
+putchar ('\n');
+
+return (0)
+
+}
